3dec91ca8dd8d870809b016d8b7ae999f2bbca40
Zemola <azeemolatunji@gmail.com>
2021-12-01 12:37:29 +0100

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-import "./style.css";
+import './style.css';
 
-const listContainer = document.querySelector("[data-lists]");
+const listContainer = document.querySelector('[data-lists]');
 const data = [
   {
     id: 1,
-    description: "Want to join meeting by 8 am ",
+    description: 'Want to join meeting by 8 am ',
     complete: false,
   },
   {
     id: 2,
-    description: "need to code with my patner",
+    description: 'need to code with my patner',
     complete: false,
   },
   {
     id: 3,
-    description: " Want to read about React",
+    description: ' Want to read about React',
     complete: false,
   },
   {
     id: 4,
-    description: "Standup meeting with my team by 6pm",
+    description: 'Standup meeting with my team by 6pm',
     complete: false,
   },
 ];
 
 const mainHandler = () => {
   data.forEach((list) => {
-    const listElement = document.createElement("li");
-    listElement.classList.add("todo");
+    const listElement = document.createElement('li');
+    listElement.classList.add('todo');
     listElement.innerHTML = `
     <div class 'check'><input type= 'checkbox'></div>
     <div class 'description'>
