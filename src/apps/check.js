b8a7cec52718ca5e0face0f16a2b6b021008c9ce
Zemola <azeemolatunji@gmail.com>
2021-12-02 18:42:07 +0100

    Rectify check and uncheck error for check.js file

--- src/apps/check.js
+++ src/apps/check.js
@@ -2,9 +2,8 @@ const checkAndUnchecklist = (todos, localStorageTodos) => {
   document.querySelectorAll('.check-box').forEach((check) => {
     check.addEventListener('change', (e) => {
       const todos = JSON.parse(localStorage.getItem(localStorageTodos));
-      const todoId = e.target.closest('li').dataset.listItem;
+      const todoId = e.target.closest('li').dataset.todoItem;
       const foundList = todos.find((list) => list.id === todoId);
-
       if (foundList.completed) {
         e.target.parentNode.nextSibling.nextElementSibling
           .querySelector('.todo-desc')
@@ -14,7 +13,7 @@ const checkAndUnchecklist = (todos, localStorageTodos) => {
         e.target.parentNode.nextSibling.nextElementSibling
           .querySelector('.todo-desc')
           .classList.add('linethrough');
-        foundTodo.completed = true;
+          foundList.completed = true;
       }
       const filteredTodo = todos.filter((todo) => todo.id !== todoId);
       filteredTodo.splice(foundList.index - 1, 0, foundList);
