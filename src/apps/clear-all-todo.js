c3767ad6b9b21d7ddf7a07cb8a18daba1109016b
Zemola <azeemolatunji@gmail.com>
2021-12-03 14:15:52 +0100

    Fix linter error

--- src/apps/clear-all-todo.js
+++ src/apps/clear-all-todo.js
@@ -3,6 +3,6 @@ const clearAllCompletedTodos = (localStorageTodos) => {
   const localArr = JSON.parse(localStorage.getItem(localStorageTodos));
   const pendingTodos = localArr.filter((list) => list.completed === false);
   localStorage.setItem(localStorageTodos, JSON.stringify(pendingTodos));
-  location.reload();
+  window.location.reload();
 };
 export default clearAllCompletedTodos;
